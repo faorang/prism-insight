36ef0020cc209ef75569c09f640d80c38ab3486e
Ubuntu <faorang@gmail.com>
2025-11-18 20:45:02 +0900

    send other logs

--- send_logs.py
+++ send_logs.py
@@ -5,8 +5,9 @@ import asyncio
 
 async def main():
     log_file = os.path.join(os.getcwd(), f"orchestrator_{datetime.now().strftime('%Y%m%d')}.log")
+    log_file2 = os.path.join(os.getcwd(), f"cut_loss_{datetime.now().strftime('%Y%m%d')}.log")
     orchestrator = StockAnalysisOrchestrator()
-    log_paths = [log_file]
+    log_paths = [log_file, log_file2]
     await orchestrator.send_telegram_messages([], log_paths)
 
 
